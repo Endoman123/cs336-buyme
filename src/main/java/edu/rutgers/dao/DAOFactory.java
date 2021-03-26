3e0f9234474b17ae573957d46648551766c8d3cc
Jared Tulayan <jared@jaredtulayan.xyz>
2021-03-26 09:43:16 -0400

    Re-introduce DAO objects

---
+++ src/main/java/edu/rutgers/dao/DAOFactory.java
@@ -0,0 +1,49 @@
+package edu.rutgers.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * This class handles creating data access objects (DAO) to interface with the datastore.
+ * 
+ * @author Mikita Belausau
+ * @author Muskan Burman
+ * @author Dorian Hobot
+ * @author Jared Tulayan
+ */
+public class DAOFactory {
+    // The information to connect to the MySQL table
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/buyme";
+    private static final String MYSQL_DATABASE_USER = "root";
+    private static final String MYSQL_DATABASE_PASSWORD = "cs336project";
+
+    // A static block for simply loading the MySQL JDBC Driver
+    static {
+        try {
+            // Load JDBC driver - the interface standardizing the connection procedure. 
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates a connection to the database
+     * 
+     * @return the {@code Connection} to the database
+     */
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(CONNECTION_URL, MYSQL_DATABASE_USER, MYSQL_DATABASE_PASSWORD);
+    }
+
+    /**
+     * Gets the DAO for users given this factory
+     * 
+     * @return the {@code UserDAO} constructed from this factory.
+     */
+    public UserDAO getUserDAO() {
+        return new UserDAO(this);
+    }
+}
