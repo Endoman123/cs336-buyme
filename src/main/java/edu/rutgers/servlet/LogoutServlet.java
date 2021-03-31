16f4a24064fbcde9f2731bed2e2bfecdbb050e55
Jared Tulayan <jared@jaredtulayan.xyz>
2021-03-30 22:24:52 -0400

    Remove doPost() method in logout

--- src/main/java/edu/rutgers/servlet/LogoutServlet.java
+++ src/main/java/edu/rutgers/servlet/LogoutServlet.java
@@ -7,29 +7,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import edu.rutgers.dao.DAOException;
-import edu.rutgers.dao.DAOFactory;
-import edu.rutgers.dao.UserDAO;
-import edu.rutgers.model.User;
 
 /**
  * Logout servlet for processing logout request
  */
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Invalidate login
         request.getSession().invalidate();
         response.sendRedirect("index.jsp");
-    }
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Postprocess request: gather and validate submitted data and display the result in the same JSP.
-        throw new ServletException("Invalid method POST");
     }
 }
