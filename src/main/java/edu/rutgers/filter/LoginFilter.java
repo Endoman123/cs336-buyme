3695e2f27e096bd1709e5ea3ee7c26d586dfe529
Jared Tulayan <jared@jaredtulayan.xyz>
2021-04-15 00:50:28 -0400

    Rework filters

--- src/main/java/edu/rutgers/filter/LoginFilter.java
+++ src/main/java/edu/rutgers/filter/LoginFilter.java
@@ -9,39 +9,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.rutgers.dao.DAOFactory;
-import edu.rutgers.dao.UserDAO;
-import edu.rutgers.model.Admin;
-import edu.rutgers.model.User;
-
 /**
- * Filter to check if a user is an admin. If they are not, redirect them to login.
+ * Filter to check if a user is logged in.
  */
-@WebFilter( urlPatterns = { "/admin", "/admin/*"} )
-public class AdminPermsFilter implements Filter {
+@WebFilter(filterName="LoginFilter")
+public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {    
-        DAOFactory daoFactory = new DAOFactory();
-        UserDAO userDao = daoFactory.getUserDAO();
-
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         
         HttpSession session = request.getSession(false);
         String loginURI = request.getContextPath() + "/login";
 
-        Admin a = userDao.getAdmin(); 
-        boolean isAdmin = session != null && session.getAttribute("user") != null && ((User) session.getAttribute("user")).equals(a);
+        boolean isLoggedIn = session != null && session.getAttribute("user") != null;
 
-        if (isAdmin)
+        if (isLoggedIn)
             chain.doFilter(request, response);
-        else
-            response.sendRedirect(loginURI);
+        else {
+            response.sendRedirect(loginURI + "?redirectURI=" + request.getRequestURI());
+        }
     }
 
     @Override
