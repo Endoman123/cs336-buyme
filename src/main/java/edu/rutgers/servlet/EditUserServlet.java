a8ff6275593691c40224cdc5b5886bfc0fe5c2bc
Jared Tulayan <jared@jaredtulayan.xyz>
2021-04-16 00:50:00 -0400

    Add EditUserServlet

---
+++ src/main/java/edu/rutgers/servlet/EditUserServlet.java
@@ -0,0 +1,95 @@
+package edu.rutgers.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.rutgers.dao.DAOFactory;
+import edu.rutgers.dao.UserDAO;
+import edu.rutgers.model.Admin;
+import edu.rutgers.model.EndUser;
+import edu.rutgers.model.User;
+import edu.rutgers.util.URLQuery;
+
+/**
+ * User servlet for editing their profile
+ */
+@WebServlet( "/profile" )
+public class EditUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DAOFactory daoFactory = new DAOFactory();
+        UserDAO userDao = daoFactory.getUserDAO();
+        String login = (String) request.getSession(false).getAttribute("login");
+
+        if (login == null)
+            throw new IllegalStateException("No active login session, was this not enforced?");
+        else  { 
+            User editUser = userDao.findEndUser(login);
+            if (editUser == null)
+                throw new ServletException("No end-user found with the login " + login);
+            else {
+                request.setAttribute("editUser", editUser);
+                request.getRequestDispatcher("/WEB-INF/views/profile.jsp").forward(request, response);
+            }
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Attempt to update the password
+        DAOFactory daoFactory = new DAOFactory();
+        UserDAO userDao = daoFactory.getUserDAO();
+        String redirectURL = request.getRequestURI() + "?" + URLQuery.encode("login", request.getParameter("loginOld"));
+
+        // Create a user
+        String newLogin = (String) request.getParameter("login"); 
+        boolean changeLogin = newLogin != null && !newLogin.isEmpty();
+        EndUser user = new EndUser();
+
+        // Use the fields from the request to set up this user
+        user.setLogin(request.getParameter("loginOld"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("password"));
+
+        changeLogin = changeLogin && !user.getLogin().equals((String) request.getParameter("loginNew"));
+
+        // Attempt to change the username, if applicable.
+        if (changeLogin)
+            userDao.updateLogin(user, request.getParameter("loginNew"));
+
+        // Attempt to update the user info
+        if (userDao.find(user.getLogin()) != null)
+            userDao.update(user);
+
+        // Update session
+        request.getSession(false).setAttribute("user", user);
+
+        response.sendRedirect(redirectURL);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DAOFactory daoFactory = new DAOFactory();
+        UserDAO userDao = daoFactory.getUserDAO();
+        HttpSession session = request.getSession(false);
+
+        User user = (User) session.getAttribute("user");
+
+        if (user != null && user instanceof Admin) {
+            userDao.delete(user);
+        }
+        
+        // Invalidate session
+        session.invalidate();
+
+        response.sendRedirect(request.getContextPath());
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/profile.jsp
@@ -0,0 +1,23 @@
+<!-- Profile View -->
+<%@ taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="tf" tagdir="/WEB-INF/tags/forms" %>
+
+<t:base title="Profile">
+    <h2>Manage ${param.login}</h2>
+    <tf:account-edit login="${editUser.login}" email="${editUser.email}" />
+    <button onclick="askDelete()">Delete Account</a>
+
+    <script>
+        function askDelete() {
+            if (confirm("Are you sure you want to delete ${editUser.login}?") == true) {
+                alert("Deleting ${editUser.login} !");
+                fetch("./support/manage-user?login=${editUser.login}", {
+                  method: "DELETE",
+                  redirect: "follow"
+                }).then(res => {
+                    self.location = res.url;
+                });
+            }
+        }    
+    </script>
+</t:base>
