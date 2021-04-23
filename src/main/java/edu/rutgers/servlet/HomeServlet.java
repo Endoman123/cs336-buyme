da6c53161a51487d71eb023a20045cfde7251b59
Jared Tulayan <jared@jaredtulayan.xyz>
2021-04-22 23:30:40 -0400

    Add navigation

--- src/main/java/edu/rutgers/servlet/HomeServlet.java
+++ src/main/java/edu/rutgers/servlet/HomeServlet.java
@@ -6,11 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import edu.rutgers.dao.DAOFactory;
-import edu.rutgers.dao.UserDAO;
-import edu.rutgers.model.User;
 
 /**
  * Main page servlet
@@ -21,33 +16,6 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        DAOFactory daoFactory = new DAOFactory();
-        UserDAO userDao = daoFactory.getUserDAO();
-        StringBuilder content = new StringBuilder();
-
-        if (session != null && session.getAttribute("user") != null) {
-            User user = (User) session.getAttribute("user");
-
-            content.append("<a href=\"support\">Support</a>");
-            content.append("<a href=\"profile\">Profile</a>");
-            content.append("<a href=\"logout\">Logout</a>");
-
-            if (userDao.getAdmin().equals(user)) { // Admin stuff
-                content.append("<a href=\"admin/add-rep\">Add Customer Representative</a>");
-            } else if (userDao.findCustomerRep(user.getLogin()) != null) { // Customer Rep Stuff
-                // not sure what to put here
-            } else { // End user stuff
-                content.append("<a href=\"autobid.jsp\">Autobid</a>");
-                content.append("<a href=\"browse.jsp\">Browse</a>");
-            }
-        } else {
-            content.append("<a href=\"login\">Login</a>");
-            content.append("<a href=\"register\">Register</a>");
-        }
-
-        request.setAttribute("content", content);
-
         request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/tags/base.tag
+++ src/main/webapp/WEB-INF/tags/base.tag
@@ -1,5 +1,6 @@
 <!-- Base Website tag -->
 <%@ tag description="Base template" pageEncoding="UTF-8"%>
+<%@ taglib prefix="u" uri="/WEB-INF/taglibs/util.tld" %>
 
 <%@ attribute name="title" type="String" %>
 <%@ attribute name="context" type="String" %>
@@ -11,12 +12,17 @@
         <base href="/cs336-buyme/"> 
         <title><% if (title != null) { %>${title} | <% } %>BuyMe</title>
     </head>
-    <header>
-        <h1>BuyMe</h1>
-    </header>
     <body>
-        <jsp:doBody/>
+        <header>
+            <a href="."><h1>BuyMe</h1></a>
+        </header>
+        <nav>
+            <u:get-navlinks />
+        </nav>
+        <main>
+            <jsp:doBody />
+        </main>
+        <footer>
+        </footer>
     </body>
-    <footer>
-    </footer>
 </html>
